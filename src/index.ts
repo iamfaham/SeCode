1749133eec22b8dae8c57ecd3835a6497d30f141
Faham <129623271+iamfaham@users.noreply.github.com>
2024-07-16 20:23:58 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import axios from 'axios';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+
+dotenv.config();
+
+const GEMINI_API_KEY: any = process.env.GEMINI_API_KEY
+const app = express();
+const port = process.env.PORT || 3000;
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+
+
+app.use(bodyParser.json());
+
+app.post('/analyze', async (req, res) => {
+  const { code } = req.body;
+
+  try {
+
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const prompt = `Analyze the following code for security vulnerabilities and provide feedback:
+        \n\n${code}\n\n 
+        Just say "looks good" or "seems correct" if the code is correct.`;
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = await response.text();
+   
+    res.json({ suggestions: text.trim() });
+
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
